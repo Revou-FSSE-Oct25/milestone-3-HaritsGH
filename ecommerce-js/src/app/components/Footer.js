5d2fac3118a287b98fcf43d4db93f2e8f65d9d73
Harits Ghiffari Hanif <harits.gh1@gmail.com>
2026-01-30 21:23:41 +0700

    cleanup: removed older folder structure

--- ecommerce-js/src/app/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="font-bold">
-        <p>copyright 2025 Harits Ghiffari Hanif. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
